4c9901e93f75751c5a116598ee439d1ef18c367f
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-10-21 00:33:43 -0300

    req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,15 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    if len(first_string) != len(second_string):
+        return False
+
+    return verify_word(first_string) == verify_word(second_string)
+
+
+def verify_word(str):
+    word = {}
+    for letter in str.lower():
+        if letter in word:
+            word[letter] += 1
+        else:
+            word[letter] = 1
+    return word
